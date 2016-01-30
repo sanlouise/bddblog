22d8da2ba1bfdd1feb7a5e50e50d36d74545ebce
sandralouise <sandra.adamshallie@gmail.com>
2016-01-30 15:19:03 +0000

    Update articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:success] = "Your article was created successfully!"
+    redirect_to articles_path
+  end
+  
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+  
+    
 end
