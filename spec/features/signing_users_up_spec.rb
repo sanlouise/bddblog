f6533263c2d3657327bab8b772d9fa53f90b003e
sandralouise <sandra.adamshallie@gmail.com>
2016-02-07 17:15:56 +0000

    Add devise views and registration path

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -5,13 +5,12 @@ RSpec.feature "Users signup" do
     visit "/"
     
     click_link "Sign Up"
-    fill_in "First Name", with: "John"
-    fill_in "Last Name", with: "Adams"
+
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
-    fill_in "Password Confirmation", with: "password"
-    click_button "Sign Up"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
     
-    expect(page).to have_content("You have signed up successfully.")
+    expect(page).to have_content("You have signed up successfully")
   end
 end
