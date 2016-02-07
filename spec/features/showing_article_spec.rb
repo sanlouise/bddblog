1fc295860c9fc3a7e3bb06a60a629f6521d1126c
sandralouise <sandra.adamshallie@gmail.com>
2016-02-07 21:04:55 +0000

    Update feature specs to match restrictions

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,14 +3,41 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
   
   before do
-    @article = Article.create(title: "The first article", body: "Body of first article")
+    @john = User.create(first_name: "John", last_name: "Adams", email: "john@example.com", password: "password")
+    @fred = User.create(first_name: "Fred", last_name: "Adams", email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Body of first article", user: @john)
+  end
+  
+  scenario "Non signed in user cannot edit or delete article" do
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "Non-owner cannot edit or delete article" do
+  login_as(@fred)
+    visit "/"
+    click_link @article.title
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "Owner can edit or delete article" do
+  login_as(@john)
+    visit "/"
+    click_link @article.title
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
   
   scenario "Display individual article" do
     visit "/"
     
     click_link @article.title
-    
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
