48937624276c9afcfef05f71fd88c7205e30ead1
sandralouise <sandra.adamshallie@gmail.com>
2016-02-07 17:35:18 +0000

    Add user sign in feature

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -5,7 +5,10 @@ RSpec.feature "Users signup" do
     visit "/"
     
     click_link "Sign Up"
-
+    
+    
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Adams"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
