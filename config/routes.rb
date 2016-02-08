428756e634cb09ca35f03583b9f6b91d4a76952b
sandralouise <sandra.adamshallie@gmail.com>
2016-02-08 12:04:30 +0000

    Update views and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: { registrations: "user/registrations" }
-
+  
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
+  
 end
